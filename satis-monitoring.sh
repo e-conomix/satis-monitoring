6e581a9468c811c2ffbcc9df1a3d2d440d2e0cf1
Andreas Schrammel <schrammel@e-conomix.at>
2017-03-01 14:48:00 +0100

    :bug: Avoid empty mails if an other then "InvalidRepositroyException" occurs

--- satis-monitoring.sh
+++ satis-monitoring.sh
@@ -5,7 +5,11 @@ export exitCode=$?;
 if [ $exitCode != 0 ];
 then
     echo "$(cat /tmp/satis | grep -A 2 'InvalidRepositoryException')" > /tmp/satisException
-    mail -s "Something went wrong exposing your packages" <your-packages-admin>@<your-company>.com < /tmp/satisException
+    export exceptionSize="$(wc -c < '/tmp/satisException')";
+    if [ $exceptionSize > 1 ];
+    then
+        mail -s "Something went wrong exposing your packages" <your-packages-admin>@<your-company>.com < /tmp/satisException
+    fi
 fi
 
 rm /tmp/satis
